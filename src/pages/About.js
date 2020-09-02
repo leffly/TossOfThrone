28d1c9f316ebc712eb6b038b41e673800a02c327
David Löffler <loffldav@fel.cvut.cz>
2020-09-02 14:16:20 +0200

    About page finished

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,8 +23,9 @@ class About extends React.Component {
                                     <img src={ThroneImg} alt="Throne of TOT" width="200" />
                                 </div>
                                 <div className="descriptionWrapper">
-                                    <p>I am LEFFLY the rightful king of Toss of throne I share my humble project with my people right here</p>
-                                    <p>Github: <a href"/"></a></p>
+                                    <p>I am LEFFLY the rightful king of Toss of throne</p>
+                                    <p>I share my humble project with my people right here</p>
+                                    <p>Github: <a href="https://github.com/leffly/TossOfThrone">https://github.com/leffly/TossOfThrone</a></p>
                                 </div>
 
                             </Panel>
